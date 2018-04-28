9320936fa7b3fe0ac839fcc670e0a5255cd36248
ICT beheer <ictbeheer@siw.nl>
2018-04-29 00:06:50 +0200

    Typo

--- includes/cookie-notification/init.php
+++ includes/cookie-notification/init.php
@@ -13,7 +13,7 @@ add_action( 'wp_footer', function() {
 			<div class="row">
 				<div class="col-md-10 cookie-text"><?php
 					esc_html_e( 'We gebruiken cookies om ervoor te zorgen dat onze website optimaal werkt en om het gebruik van onze website te analyseren.', 'siw' ); echo SPACE;
-					esc_html_e( ' Door gebruik te blijven maken van onze website, ga je hiermee akkoord.', 'siw' ); ?>
+					esc_html_e( 'Door gebruik te blijven maken van onze website, ga je hiermee akkoord.', 'siw' ); ?>
 				</div>
 				<div class="col-md-2 cookie-button">
 					<button id="siw-cookie-consent" class="button"><?php esc_html_e( 'Ik ga akkoord!', 'siw' ); ?></button>
