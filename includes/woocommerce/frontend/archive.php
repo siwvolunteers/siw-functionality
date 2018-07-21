c65f17e2466f6756830c15ff5d9ef5ae96e98ad9
ICT beheer <ictbeheer@siw.nl>
2018-07-21 20:30:32 +0200

    Kortingsactie in topbar en overzichtspagina

--- includes/woocommerce/frontend/archive.php
+++ includes/woocommerce/frontend/archive.php
@@ -3,7 +3,7 @@
 (c)2017-2018 SIW Internationale Vrijwilligersprojecten
 */
 if ( ! defined( 'ABSPATH' ) ) {
-	exit; // Exit if accessed directly
+	exit;
 }
 
 /*
@@ -48,7 +48,7 @@ add_filter( 'yith_wcan_get_terms_list', function ( $terms, $taxonomy, $instance 
 }, 10, 3 );
 
 
-/* */
+/* Verberg AJAX-filter van land, doelgroep of maand op desbetreffende landingspagina */
 add_filter( 'sidebars_widgets', function( $sidebars_widgets ) {
 	global $wp_query;
 
@@ -190,16 +190,35 @@ add_action( 'after_page_header', function() {
 		return;
 	}
 
+	/* Toon algemene uitleg over groepsprojecten */
 	$text .= SPACE .
 		__( 'Tijdens onze Groepsprojecten ga je samen met een internationale groep vrijwilligers voor 2 á 3 weken aan de slag.', 'siw' ) . SPACE .
 		__( 'De projecten hebben vaste begin- en einddata.', 'siw' ) . SPACE .
 		sprintf( __( 'We vertellen je meer over de werkwijze van deze projecten op onze pagina <a href="%s">Groepsprojecten</a>.', 'siw' ), esc_url( $workcamps_page_link ) );
 
+	/* Toon aankondiging voor nieuwe projecten*/
 	if ( siw_get_setting( 'workcamp_teaser_text_enabled' ) && date('Y-m-d') <= siw_get_setting( 'workcamp_teaser_text_end_date' ) ) {
 		$teaser_text_end_year = date( 'Y', strtotime( siw_get_setting( 'workcamp_teaser_text_end_date' ) ) );
 		$text .= BR2 . sprintf( __( 'Vanaf maart wordt het aanbod aangevuld met honderden nieuwe vrijwilligersprojecten voor %s.', 'siw' ), $teaser_text_end_year ). SPACE .
 			__( 'Wil je nu al meer weten over de grensverleggende mogelijkheden van SIW?', 'siw' ) . SPACE .
 			sprintf( __( '<a href="%s">Bel of mail ons</a> en we denken graag met je mee!', 'siw' ), esc_url( $contact_page_link ) );
+	}
+
+	/* Toon extra tekst als de kortingsactie actief is */
+	if ( siw_is_sale_active() ) {
+		/* Ophalen tarieven en einddatum */
+		$tariffs = siw_get_workcamp_tariffs();
+		$regular = siw_format_amount( $tariffs[ 'regulier' ] );
+		$regular_sale = siw_format_amount( $tariffs[ 'regulier_aanbieding' ] );
+		$student = siw_format_amount( $tariffs[ 'student' ] );
+		$student_sale = siw_format_amount( $tariffs[ 'student_aanbieding' ] );
+		$end_date = siw_get_date_in_text( siw_get_setting( 'workcamp_sale_end_date' ), false );
+
+		$text .= BR2 . sprintf( __( 'Meld je nu aan en betaal geen %s maar %s voor je vrijwilligersproject.', 'siw' ), $regular, '<b>'. $regular_sale .'</b>' ) . SPACE .
+			__( 'Ben je student of jonger dan 18 jaar?', 'siw' ) . SPACE .
+			sprintf( __( 'Dan betaal je in plaats van %s nog maar %s.', 'siw' ), $student, '<b>'. $student_sale .'</b>' ) . BR  .
+			'<b>' . __( 'Let op:', 'siw' ) . '</b>' . SPACE .
+			sprintf( __( 'Deze actie duurt nog maar t/m %s, dus wees er snel bij.', 'siw' ), $end_date );
 	}
 
 ?>
