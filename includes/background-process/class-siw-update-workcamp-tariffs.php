b6b4ce2fc15c2765f6e18cd336bb8670e0a9aa26
ICT beheer <ictbeheer@siw.nl>
2019-02-26 21:51:07 +0100

    Update bijwerken tarieven

--- includes/background-process/class-siw-update-workcamp-tariffs.php
+++ includes/background-process/class-siw-update-workcamp-tariffs.php
@@ -7,31 +7,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Proces om tarieven van Groepsprojecten bij te werken
  * 
- * @package SIW\Background-Process
- * @author Maarten Bruna
- * @copyright 2017-2018 SIW Internationale Vrijwilligersprojecten
- * @uses siw_update_workcamp_tariff()
+ * @package   SIW\Background-Process
+ * @author    Maarten Bruna
+ * @copyright 2017-2019 SIW Internationale Vrijwilligersprojecten
+ * @uses      SIW_Util
  */
 class SIW_Update_Workcamp_Tariffs extends SIW_Background_Process {
 
 	/**
-	 * @var string
+	 * {@inheritDoc}
 	 */
 	protected $action = 'update_workcamp_tariffs_process';
 
 	/**
-	 * @var string
+	 * {@inheritDoc}
 	 */
 	protected $name = 'bijwerken tarieven';
 
 	/**
-	 * Selecteer alle zichtbare projecten //TODO: misschien gewoon alle projecten selecteren?
+	 * Selecteer alle zichtbare projecten
 	 *
 	 * @return array
 	 */
 	protected function select_data() {
 		$args = [
-			'visibility' => 'visible',
 			'return'     => 'ids',
 			'limit'      => -1,
 		];
@@ -43,15 +42,54 @@ class SIW_Update_Workcamp_Tariffs extends SIW_Background_Process {
 	/**
 	 * Werk tarieven van het groepsproject bij
 	 *
-	 * @param mixed $item
+	 * @param int $product_id
 	 *
 	 * @return mixed
 	 */
-	protected function task( $item ) {
+	protected function task( $product_id ) {
 
-		if ( siw_update_workcamp_tariff( $item ) ) {
-			$this->increment_processed_count();
+		$tariffs = [
+			'regulier' => [
+				'name'          => 'regulier',
+				'regular_price' => SIW_Properties::WORKCAMP_FEE_REGULAR,
+				'sale_price'    => SIW_Properties::WORKCAMP_FEE_REGULAR_SALE
+			],
+			'student' => [
+				'name'          => 'student / <18',
+				'regular_price' => SIW_Properties::WORKCAMP_FEE_STUDENT,
+				'sale_price'    => SIW_Properties::WORKCAMP_FEE_STUDENT_SALE
+			]
+		];
+
+		$product = wc_get_product( $product_id );
+	
+		/* Afbreken als product niet meer bestaat */
+		if ( false == $product ) {
+			return false;
 		}
+	
+		$sale = SIW_Util::is_workcamp_sale_active();
+
+		$variations = $product->get_children();
+	
+		foreach ( $variations as $variation_id ) {
+			$variation = wc_get_product( $variation_id );
+			$variation_tariff = $variation->get_attributes()['pa_tarief'];
+			$tariff = $tariffs[ $variation_tariff ];
+
+			$regular_price = $tariff['regular_price'];
+			$sale_price = $tariff['sale_price'];
+
+			$variation->set_props([
+				'regular_price'     => $regular_price,
+				'sale_price'        => $sale ? $sale_price : null,
+				'price'             => $sale ? $sale_price : $regular_price,
+				'date_on_sale_from' => $sale ? date( DATE_ISO8601, strtotime( siw_get_setting( 'workcamp_sale_start_date' ) ) ) : null,
+				'date_on_sale_to'   => $sale ? date( DATE_ISO8601, strtotime( siw_get_setting( 'workcamp_sale_end_date' ) ) ) : null,
+			]);
+			$variation->save();
+		}
+		$this->increment_processed_count();
 		return false;
 	}
 }
