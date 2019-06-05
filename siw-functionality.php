a50e8d98d0f9814d310fe0ef9c5f97efaedf9d3b
ICT beheer <ictbeheer@siw.nl>
2019-06-05 23:30:50 +0200

    2.1.0

--- siw-functionality.php
+++ siw-functionality.php
@@ -1,4 +1,9 @@
 <?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 /**
  * SIW Functionaliteit
  *
@@ -10,13 +15,13 @@
  * Plugin Name: SIW Functionaliteit
  * Plugin URI:  https://github.com/siwvolunteers/siw-functionality
  * Description: Extra functionaliteit t.b.v website SIW
- * Version:     2.0.4
+ * Version:     2.1.0
  * Author:      Maarten Bruna
  * Text Domain: siw
  */
 
 /** Constantes */
-define ( 'SIW_PLUGIN_VERSION', '2.0.4' );
+define ( 'SIW_PLUGIN_VERSION', '2.1.0' );
 define ( 'SIW_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define ( 'SIW_ASSETS_DIR', SIW_PLUGIN_DIR . 'assets' );
 define ( 'SIW_TEMPLATES_DIR', SIW_PLUGIN_DIR . 'templates' );
