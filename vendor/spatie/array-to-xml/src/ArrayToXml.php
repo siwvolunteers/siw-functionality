28e912e32551f545370f6aa18205eb216f73809a
ICT beheer <ictbeheer@siw.nl>
2019-07-25 22:32:37 +0200

    Update dependencies

--- vendor/spatie/array-to-xml/src/ArrayToXml.php
+++ vendor/spatie/array-to-xml/src/ArrayToXml.php
@@ -2,10 +2,10 @@
 
 namespace Spatie\ArrayToXml;
 
+use Exception;
 use DOMElement;
 use DOMDocument;
 use DOMException;
-use Exception;
 
 class ArrayToXml
 {
@@ -77,7 +77,8 @@ class ArrayToXml
         return $this->document;
     }
 
-    protected function ensureValidDomProperties(array $domProperties) {
+    protected function ensureValidDomProperties(array $domProperties)
+    {
         foreach ($domProperties as $key => $value) {
             if (! property_exists($this->document, $key)) {
                 throw new Exception($key.' is not a valid property of DOMDocument');
@@ -168,7 +169,7 @@ class ArrayToXml
 
     protected function addSequentialNode(DOMElement $element, $value)
     {
-        if (empty($element->nodeValue)) {
+        if (empty($element->nodeValue) && ! is_numeric($element->nodeValue)) {
             $element->nodeValue = htmlspecialchars($value);
 
             return;
