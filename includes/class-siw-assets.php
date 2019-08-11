1c86087480598a3d2a7c62ef9f5815d30d48529f
ICT beheer <ictbeheer@siw.nl>
2019-08-11 14:12:05 +0200

    JS Cookie en smoothscroll

--- includes/class-siw-assets.php
+++ includes/class-siw-assets.php
@@ -4,10 +4,20 @@
  * Class om scripts en styles te registreren
  * 
  * @package     SIW
- * @copyright   2018 SIW Internationale Vrijwilligersprojecten
+ * @copyright   2018-2019 SIW Internationale Vrijwilligersprojecten
  * @author      Maarten Bruna
  */
 class SIW_Assets {
+
+	/**
+	 * Versie van JS Cookie
+	 */
+	const JSCOOKIE_VERSION = '2.2.1';
+
+	/**
+	 * Versie van SmoothScroll
+	 */
+	const SMOOTHSCROLL_VERSION = '1.4.9';
 
 	/**
 	 * Init
@@ -30,6 +40,10 @@ class SIW_Assets {
 	 * Registreert scripts
 	 */
 	public function register_scripts() {
-	}
+		//JS-cookie niet zelf enqueuen; is dependency van andere scripts
+		wp_register_script( 'js-cookie', SIW_ASSETS_URL . 'modules/js-cookie/js.cookie.js', [], self::JSCOOKIE_VERSION, true );
 
+		wp_register_script( 'smoothscroll', SIW_ASSETS_URL . 'modules/smoothscroll/smoothscroll.js', [], self::SMOOTHSCROLL_VERSION, true );
+		wp_enqueue_script('smoothscroll' );
+	}
 }
