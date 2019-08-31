558ea96e6eda35fde2dcb2783547e9ea05bef9bb
ICT beheer <ictbeheer@siw.nl>
2019-08-31 16:07:53 +0200

    Create siw-charts.js

---
+++ assets/js/siw-charts.js
@@ -0,0 +1,35 @@
+/**
+ * @file      Functies t.b.v. grafieken
+ * @author    Maarten Bruna 
+ * @copyright 2019 SIW Internationale Vrijwilligersprojecten
+ */
+
+siwChartsInitAll();
+
+/**
+ * Initialiseert alle grafieken
+ */
+function siwChartsInitAll() {
+	//Zoek alle grafieken
+	var charts = document.querySelectorAll( '.siw-chart' );
+
+	//Intialiseer elke grafiek
+	charts.forEach( function ( el, i ) {
+		siwChartsInitSingle( el );
+	})
+}
+
+/**
+ * Initialiseert een grafiek
+ *
+ * @param {Element} el
+ */
+function siwChartsInitSingle( el ) {
+	var options = JSON.parse( el.dataset.options );
+
+	var chart = new ApexCharts(
+		el,
+		options
+	);
+	chart.render();
+}
