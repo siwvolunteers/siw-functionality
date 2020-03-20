fc377ccac1b4b448f50a8ac5b04021ccf86438d6
ICT beheer <ictbeheer@siw.nl>
2020-03-21 00:56:21 +0100

    Update quote widget

--- includes/widgets/quote/quote.php
+++ includes/widgets/quote/quote.php
@@ -5,7 +5,7 @@ namespace SIW\Widgets;
 /**
  * Widget met quote
  *
- * @copyright 2019 SIW Internationale Vrijwilligersprojecten
+ * @copyright 2019-2020 SIW Internationale Vrijwilligersprojecten
  * @since     3.0.0
  * 
  * @widget_data
@@ -51,7 +51,7 @@ class Quote extends Widget {
 			'category' => [
 				'type'    => 'select',
 				'label'   => __( 'Categorie', 'siw'),
-				'options' => siw_get_testimonial_quote_categories(),
+				'options' => $this->get_categories(),
 			]
 		];
 		return $widget_form;
@@ -61,7 +61,7 @@ class Quote extends Widget {
 	 * {@inheritDoc}
 	 */
 	protected function get_content( array $instance, array $args, array $template_vars, string $css_name ) {
-		$quote = siw_get_testimonial_quote( $instance['category'] );
+		$quote = $this->get_quote( $instance['category'] );
 		ob_start();
 		?>
 		<blockquote>
@@ -72,4 +72,51 @@ class Quote extends Widget {
 		$content = ob_get_clean();
 		return $content;
 	}
+
+	/**
+	 * Geeft lijst van categorieën voor quotes terug
+	 *
+	 * @return array
+	 */
+	function get_categories() {
+		$groups = get_terms( 'testimonial-group' );
+		$categories[''] = __( 'Alle', 'siw' );
+		foreach ( $groups as $group ) {
+			$categories[ $group->slug ] = $group->name;
+		}
+		return $categories;
+	}
+
+	/**
+	 * Geeft array met gegevens van een quote terug
+	 *
+	 * @param  string $category
+	 * @return array
+	 */
+	function get_quote( $category = '' ) {
+
+		$query_args = [
+			'post_type'           => 'testimonial',
+			'posts_per_page'      => 1,
+			'post_status'         => 'publish',
+			'orderby'             => 'rand',
+			'fields'              => 'ids',
+			'testimonial-group'   => $category,
+		];
+		$post_ids = get_posts( $query_args );
+
+		if ( empty( $post_ids ) ) {
+			return;
+		}
+
+		$post_id = $post_ids[0];
+		$quote = [
+			'quote'   => get_post_field('post_content', $post_id ),
+			'name'    => get_the_title( $post_id ),
+			'project' => get_post_meta( $post_id, '_kad_testimonial_location', true ),
+		];
+		return $quote;
+	}
+
+
 }
