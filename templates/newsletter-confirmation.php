aa1ccb02ead124f8848b406275aa9dcbc3211def
ICT beheer <ictbeheer@siw.nl>
2020-06-07 18:26:22 +0200

    template nieuwsbriefpagina

--- templates/newsletter-confirmation.php
+++ templates/newsletter-confirmation.php
@@ -1,10 +1,92 @@
-<?php get_header();?>
-<div id="content" class="siw-newsletter-confirmation">
-	<div class="row">
-		<div class="main <?php echo kadence_main_class(); ?>" role="main">
-			<h4 class="sectiontitle"><?php esc_html_e( 'Aanmelding nieuwsbrief', 'siw'); ?></h4>
-			<p>
-				<?php do_action( 'siw_newsletter_confirmation' );?>
-			</p>
-		</div>
-<?php get_footer(); ?>
+<?php
+
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package GeneratePress
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+	<div id="primary" <?php generate_do_element_classes( 'content' ); ?>>
+		<main id="main" <?php generate_do_element_classes( 'main' ); ?>>
+			<?php
+			/**
+			 * generate_before_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_before_main_content' );
+			?>
+
+			<div class="inside-article">
+
+				<?php
+				/**
+				 * generate_before_content hook.
+				 *
+				 * @since 0.1
+				 *
+				 * @hooked generate_featured_page_header_inside_single - 10
+				 */
+				do_action( 'generate_before_content' );
+				?>
+
+				<header class="entry-header">
+					<h1 class="entry-title" itemprop="headline"><?php esc_html_e( 'Aanmelding nieuwsbrief', 'siw'); ?></h1>
+				</header><!-- .entry-header -->
+
+				<?php
+				/**
+				 * generate_after_entry_header hook.
+				 *
+				 * @since 0.1
+				 *
+				 * @hooked generate_post_image - 10
+				 */
+				do_action( 'generate_after_entry_header' );
+				?>
+
+				<div class="entry-content" itemprop="text">
+					<?php
+					do_action( 'siw_newsletter_confirmation' );
+					?>
+				</div><!-- .entry-content -->
+
+				<?php
+				/**
+				 * generate_after_content hook.
+				 *
+				 * @since 0.1
+				 */
+				do_action( 'generate_after_content' );
+				?>
+
+			</div><!-- .inside-article -->
+
+			<?php
+			/**
+			 * generate_after_main_content hook.
+			 *
+			 * @since 0.1
+			 */
+			do_action( 'generate_after_main_content' );
+			?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+	<?php
+	/**
+	 * generate_after_primary_content_area hook.
+	 *
+	 * @since 2.0
+	 */
+	do_action( 'generate_after_primary_content_area' );
+
+	generate_construct_sidebars();
+
+get_footer();
