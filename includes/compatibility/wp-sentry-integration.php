a296957d18cd6750be790d0b362ef2a7fbbe1195
ICT beheer <ictbeheer@siw.nl>
2020-11-10 20:54:18 +0100

    update WP Sentry compat

--- includes/compatibility/wp-sentry-integration.php
+++ includes/compatibility/wp-sentry-integration.php
@@ -39,10 +39,9 @@ class WP_Sentry_Integration {
 	public function define_constants() {
 		$constants = [
 			'WP_SENTRY_VERSION'     => SIW_PLUGIN_VERSION,
-			'WP_SENTRY_ENV'         => \wp_get_environment_type(),
 			'WP_SENTRY_ERROR_TYPES' => E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_USER_DEPRECATED,
-			'WP_SENTRY_PUBLIC_DSN'  => self::JS_DSN,
-			'WP_SENTRY_DSN'         => self::PHP_DSN,
+			'WP_SENTRY_BROWSER_DSN' => self::JS_DSN,
+			'WP_SENTRY_PHP_DSN'     => self::PHP_DSN,
 			'WP_SENTRY_DEFAULT_PII' => true,
 		];
 
