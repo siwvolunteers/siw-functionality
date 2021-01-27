230a0b6ad9054672b3dbddde18e6b00542868cfb
ICT beheer <ictbeheer@siw.nl>
2021-01-27 21:11:13 +0100

    WP Sentry configuratie naar mu-plugin

--- includes/compatibility/wp-sentry-integration.php
+++ includes/compatibility/wp-sentry-integration.php
@@ -10,38 +10,14 @@ namespace SIW\Compatibility;
  */
 class WP_Sentry_Integration {
 
-	/** PHP-DSN */
-	const PHP_DSN = 'https://d66e53bd9d3e41199ff984851c98706b@sentry.io/1264830';
-
-	/** JS-DSN */
-	const JS_DSN = 'https://e8240c08387042d583692b6415c700e3@sentry.io/1264820';
-
 	/** Init */
 	public static function init() {
 		$self = new self();
-		$self->define_constants();
-		add_filter( 'rocket_exclude_js', [ $self, 'exclude_js_from_combine' ] );
-	}
-
-	/** Definieer constantes voor WP Sentry */
-	public function define_constants() {
-		$constants = [
-			'WP_SENTRY_VERSION'     => SIW_PLUGIN_VERSION,
-			'WP_SENTRY_ERROR_TYPES' => E_ALL & ~E_DEPRECATED & ~E_NOTICE & ~E_USER_DEPRECATED,
-			'WP_SENTRY_BROWSER_DSN' => self::JS_DSN,
-			'WP_SENTRY_PHP_DSN'     => self::PHP_DSN,
-			'WP_SENTRY_DEFAULT_PII' => true,
-		];
-
-		foreach ( $constants as $constant => $value ) {
-			if ( ! defined( $constant ) && ! empty( $value ) ) {
-				define( $constant, $value );
-			}
-		}
+		add_filter( 'rocket_exclude_js', [ $self, 'exclude_js' ] );
 	}
 
 	/** JS-bestanden uitsluiten van minification/concatenation */
-	public function exclude_js_from_combine( array $excluded_files ) : array {
+	public function exclude_js( array $excluded_files ) : array {
 		$excluded_files[] = '/wp-content/plugins/wp-sentry-integration/public/(.*).js';
 		return $excluded_files;
 	}
