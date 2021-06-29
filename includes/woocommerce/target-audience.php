07afa3342f450ec5e8d49f1254a9bf45ffb42b2e
ICT beheer <ictbeheer@siw.nl>
2021-06-29 22:49:07 +0200

    Enum voor doelgroep

---
+++ includes/woocommerce/target-audience.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace SIW\WooCommerce;
+
+use \Spatie\Enum\Enum;
+
+/**
+ * Doelgroepen
+ * 
+ * @copyright 2021 SIW Internationale Vrijwilligersprojecten
+ * 
+ * @method static self FAMILIES()
+ * @method static self TEENAGERS()
+ */
+class Target_Audience extends Enum {
+
+	/** {@inheritDoc} */
+	protected static function values(): array {
+		return [
+			'FAMILIES'  => 'families',
+			'TEENAGERS' => 'tieners',
+		];
+		
+	}
+
+	/** {@inheritDoc} */
+	protected static function labels(): array {
+		return [
+			'FAMILIES'  => __( 'Families', 'siw' ),
+			'TEENAGERS' => __( 'Tieners', 'siw' ),
+		];
+	}
+}
