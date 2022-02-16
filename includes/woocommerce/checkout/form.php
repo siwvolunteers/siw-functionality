73ef8c72b8b880abac05361475f2ed9e789b1035
ICT beheer <ictbeheer@siw.nl>
2022-02-16 11:52:24 +0100

    Geen aparte class voor radio inputs

--- includes/woocommerce/checkout/form.php
+++ includes/woocommerce/checkout/form.php
@@ -12,7 +12,6 @@ class Form {
 	/** Init */
 	public static function init() {
 		$self = new self();
-		add_filter( 'woocommerce_form_field_args', [ $self, 'add_form_field_classes' ] );
 		add_action( 'woocommerce_multistep_checkout_before_order_info', [ $self, 'show_checkout_partner_fields'] );
 		add_filter( 'woocommerce_checkout_cart_item_quantity', '__return_empty_string' );
 		remove_action( 'woocommerce_checkout_terms_and_conditions', 'wc_checkout_privacy_policy_text', 20 );
@@ -50,13 +49,5 @@ class Form {
 			<?php endforeach ?>
 		</div>
 		<?php
-	}
-
-	/** Voegt extra classes voor gestylde radiobuttons, checkboxes en selects toe */
-	public function add_form_field_classes( array $args ) : array {
-		if ( $args['type'] == 'radio' ) {
-			$args['class'][] = 'radio-icon';
-		}
-		return $args;
 	}
 }
