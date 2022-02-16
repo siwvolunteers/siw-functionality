18004034d6f347a2eaf69314088f4c0e4009db3d
ICT beheer <ictbeheer@siw.nl>
2022-02-16 11:50:02 +0100

    Postcode lookup naar eigen class

--- includes/woocommerce/checkout/form.php
+++ includes/woocommerce/checkout/form.php
@@ -12,7 +12,6 @@ class Form {
 	/** Init */
 	public static function init() {
 		$self = new self();
-		add_action( 'wp_enqueue_scripts', [ $self, 'add_postcode_script' ] );
 		add_filter( 'woocommerce_form_field_args', [ $self, 'add_form_field_classes' ] );
 		add_action( 'woocommerce_multistep_checkout_before_order_info', [ $self, 'show_checkout_partner_fields'] );
 		add_filter( 'woocommerce_checkout_cart_item_quantity', '__return_empty_string' );
@@ -51,24 +50,6 @@ class Form {
 			<?php endforeach ?>
 		</div>
 		<?php
-	}
-
-	/** Voegt inline script voor postcode lookup toe */
-	public function add_postcode_script() {
-
-		wp_register_script( 'siw-checkout-postcode-lookup', SIW_ASSETS_URL . 'js/siw-checkout-postcode-lookup.js', ['siw-api-postcode-lookup'], SIW_PLUGIN_VERSION, true );
-
-		$postcode_selectors = [
-			'postcode'    => "billing_postcode",
-			'housenumber' => "billing_housenumber",
-			'street'      => "billing_address_1",
-			'city'        => "billing_city",
-		];
-		wp_localize_script( 'siw-checkout-postcode-lookup', 'siw_checkout_postcode_selectors', $postcode_selectors );
-
-		if ( is_checkout() && ! is_order_received_page() && ! is_checkout_pay_page() ) {
-			wp_enqueue_script( 'siw-checkout-postcode-lookup' );
-		}
 	}
 
 	/** Voegt extra classes voor gestylde radiobuttons, checkboxes en selects toe */
--- includes/woocommerce/loader.php
+++ includes/woocommerce/loader.php
@@ -28,6 +28,7 @@ class Loader extends Class_Loader_Abstract {
 			Checkout\Discount::class,
 			Checkout\Fields::class,
 			Checkout\Form::class,
+			Checkout\Postcode_Lookup::class,
 			Checkout\Newsletter::class,
 			Checkout\Validation::class,
 
