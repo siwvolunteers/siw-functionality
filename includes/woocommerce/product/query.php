ea070bfe951f773ffb5df44838c1b0222e746945
ICT beheer <ictbeheer@siw.nl>
2022-03-06 21:15:31 +0100

    Overbodige witregels verwijdren

--- includes/woocommerce/product/query.php
+++ includes/woocommerce/product/query.php
@@ -13,9 +13,7 @@ class Query {
 
 	/** Init */
 	public static function init() {
-
 		$self = new self();
-
 		add_filter( 'woocommerce_product_data_store_cpt_get_products_query', [ $self, 'enable_project_id_search' ], 10, 2 );
 		add_filter( 'woocommerce_product_data_store_cpt_get_products_query', [ $self, 'enable_country_search' ], 10, 2 );
 	}
