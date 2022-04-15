442aabea72c9898dabd11198d25ccbff784957a4
ICT beheer <ictbeheer@siw.nl>
2022-04-15 11:21:35 +0200

    Gegevens vullen bij update projecttype

--- includes/actions/batch/update-product-type.php
+++ includes/actions/batch/update-product-type.php
@@ -4,9 +4,10 @@ namespace SIW\Actions\Batch;
 
 use SIW\Interfaces\Actions\Batch as Batch_Action_Interface;
 use SIW\WooCommerce\Product\WC_Product_Project;
+use SIW\WooCommerce\Product_Attribute;
 
 /**
- * Proces om product type te updaten (van variable naar projectd
+ * Proces om product type te updaten (van variable naar project)
  * 
  * @copyright 2022 SIW Internationale Vrijwilligersprojecten
  */
@@ -45,7 +46,7 @@ class Update_Product_Type implements Batch_Action_Interface {
 	public function process( $product_id ) {
 		$product = wc_get_product( $product_id );
 		if ( ! is_a( $product, \WC_Product_Variable::class ) ) {
-			//return;
+			return;
 		}
 
 		//Variaties verwijderen
@@ -55,15 +56,14 @@ class Update_Product_Type implements Batch_Action_Interface {
 			$variation->delete( true );
 		}
 
-		//Type aanpassen
-		wp_set_object_terms( $product_id, WC_Product_Project::PRODUCT_TYPE, 'product_type' );
-
 		//Meta migreren
 		$meta_keys = [
 			'project_id'                 => '_project_id',
+			'checksum'                   => '_checksum',
 			'deleted_from_plato'         => '_deleted_from_plato',
 			'force_hide'                 => '_hidden',
 			'use_stockphoto'             => '_use_stockphoto',
+			'has_plato_image'            => '_has_plato_image',
 			'start_date'                 => '_start_date',
 			'approval_result'            => '_approval_result',
 			'description'                => '_project_description',
@@ -73,15 +73,35 @@ class Update_Product_Type implements Batch_Action_Interface {
 			'participation_fee'          => '_participation_fee',
 			'latitude'                   => '_latitude',
 			'longitude'                  => '_longitude',
+			'project_is_full'            => '_full',
 		];
 
-		foreach ( $meta_keys as $old => $new ) {
-			$product->update_meta_data( $new, $product->get_meta( $old ) );
-			$product->delete_meta_data( $old );
+		foreach ( $meta_keys as $old_meta => $new_meta ) {
+			$new_value = $product->get_meta( $old_meta );
+			if ( ! empty( $new_value ) ) {
+				$product->update_meta_data( $new_meta, $new_value );
+			}
+			$product->delete_meta_data( $old_meta );
 		}
+
+		//Vul einddatum o.b.v. product attribute
+		$product->update_meta_data( '_end_date', date( 'Y-m-d', strtotime( $product->get_attribute( Product_Attribute::END_DATE()->label ) ) ) );
+
+		//Overschrijf naam
+		$product->set_name( $product->get_attribute( Product_Attribute::PROJECT_NAME() ) );
 
 		//SEO-meta verwijderen
 		$product->delete_meta_data( '_genesis_title' );
 		$product->delete_meta_data( '_genesis_description' );
+
+		//Overbodige velden leegmaken
+		$product->delete_meta_data( 'country' );
+		$product->set_short_description( null );
+		$product->set_default_attributes([]);
+		$product->save();
+
+		//Type aanpassen 
+		$product = new WC_Product_Project( $product_id );
+		$product->save();
 	}
 }
