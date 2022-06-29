c3251dd1dd9032cd02d87950d2321f82a368bd85
ICT beheer <ictbeheer@siw.nl>
2022-06-29 18:40:29 +0200

    Default max redirects is 1000

--- includes/compatibility/safe-redirect-manager.php
+++ includes/compatibility/safe-redirect-manager.php
@@ -10,17 +10,12 @@ namespace SIW\Compatibility;
  */
 class Safe_Redirect_Manager {
 
-	/** Maximaal aantal redirects */
-	const MAX_REDIRECTS = 250;
-
 	/** Init */
 	public static function init() {
 
 		if ( ! is_plugin_active( 'safe-redirect-manager/safe-redirect-manager.php' ) ) {
 			return;
 		}
-
-		add_filter( 'srm_max_redirects', fn(): int => self::MAX_REDIRECTS );
 		add_filter( 'srm_default_direct_status', fn(): int => \WP_Http::MOVED_PERMANENTLY );
 	}
 }
