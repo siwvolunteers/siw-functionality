7192af43e9cf6c59a68dff07b7070035fa8c66f4
ICT beheer <ictbeheer@siw.nl>
2022-07-02 21:39:34 +0200

    Assets ook in admin laden

--- includes/assets/asset.php
+++ includes/assets/asset.php
@@ -16,11 +16,13 @@ class Asset {
 	/** Voegt style toe */
 	public function register_style( Style $style ) {
 		add_action( 'wp_enqueue_scripts', [ $style, 'register_style' ] );
+		add_action( 'admin_enqueue_scripts', [ $style, 'register_style' ] );
 	}
 
 	/** Voegt script toe */
 	public function register_script( Script $script ) {
 		add_action( 'wp_enqueue_scripts', [ $script, 'register_script' ] );
+		add_action( 'admin_enqueue_scripts', [ $script, 'register_script' ] );
 	}
 
 	/** Registeer asset als extern (voor prefetch en uitsluiten van optimalisatie) */
