d78554b96bddeb257aab5f24182de5675b433c01
ICT beheer <ictbeheer@siw.nl>
2022-08-11 22:30:51 +0200

    Aparte add to cart button

--- includes/woocommerce/frontend/product.php
+++ includes/woocommerce/frontend/product.php
@@ -18,9 +18,11 @@ class Product {
 	public static function init() {
 		$self = new self();
 		add_filter( 'woocommerce_display_product_attributes', [ $self, 'display_product_attributes' ], 10, 2 );
-		add_action( 'woocommerce_project_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
+		add_action( 'woocommerce_project_add_to_cart', [ $self, 'project_add_to_cart' ], 30 );
 
 		add_action( 'woocommerce_single_product_summary', [ $self, 'show_project_summary' ], 20 );
+
+		add_filter( 'wc_get_template', [ $self, 'set_template' ], 10, 5 ); // TODO: generiek maken
 
 		add_action( 'woocommerce_before_single_product_summary', [ $self, 'show_featured_badge' ], 10 );
 		add_filter( 'woocommerce_price_trim_zeros', '__return_true' ); // TODO: verplaatsen naar compat
@@ -116,5 +118,18 @@ class Product {
 		if ( $product->is_featured() && ! $product->is_on_sale() ) {
 			echo '<span class="product-badge featured-badge">' . esc_html__( 'Aanbevolen', 'siw' ) . '</span>';
 		}
+	}
+
+	/** Voegt Add to cart button toe */
+	public function project_add_to_cart() {
+		wc_get_template( 'single-product/add-to-cart/project.php' );
+	}
+
+	/** Overschrijft template */
+	public function set_template( string $located, string $template_name, array $args, string $template_path, string $default_path ): string {
+		if ( 'single-product/add-to-cart/project.php' === $template_name ) {
+			$located = SIW_TEMPLATES_DIR . 'woocommerce/' . $template_name;
+		}
+		return $located;
 	}
 }
