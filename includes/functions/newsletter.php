69a7d469622fd33ad58213935771cfd078e3ecfb
ICT beheer <14947039+ictbeheer@users.noreply.github.com>
2022-12-18 15:57:20 +0100

    Ongebruikte functie verwijderen

--- includes/functions/newsletter.php
+++ includes/functions/newsletter.php
@@ -10,19 +10,6 @@ use SIW\External\Mailjet;
  * @todo evt class met static functions van maken
  */
 
-/** Geeft lijst met nieuwsbrieven terug */
-function siw_newsletter_get_lists( bool $include_subscriber_count = true ): array {
-	$mailjet = new Mailjet();
-	$mailjet_lists = $mailjet->get_lists();
-
-	$lists = [];
-	foreach ( $mailjet_lists as $list ) {
-		$lists[ $list['id'] ] = $include_subscriber_count ? "{$list['name']} ({$list['subscriber_count']})" : $list['name'];
-	}
-
-	return $lists;
-}
-
 /** Abonnee aanmelden voor nieuwsbrief */
 function siw_newsletter_subscribe( string $email, int $list_id, array $properties = [] ): bool {
 	$mailjet = new Mailjet();
