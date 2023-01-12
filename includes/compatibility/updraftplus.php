728c20e53a8ad4bb7c35656d4c900123f7e0ee2b
ICT beheer <14947039+ictbeheer@users.noreply.github.com>
2023-01-12 20:54:45 +0100

    Updraftplus met baseclass

--- includes/compatibility/updraftplus.php
+++ includes/compatibility/updraftplus.php
@@ -2,32 +2,33 @@
 
 namespace SIW\Compatibility;
 
+use SIW\Attributes\Action;
+use SIW\Attributes\Filter;
+use SIW\Base;
+
 /**
  * Aanpassingen voor UpdraftPlus
  *
  * @copyright 2019-2021 SIW Internationale Vrijwilligersprojecten
  * @see       https://updraftplus.com/
  */
-class UpdraftPlus {
+class UpdraftPlus extends Base {
 
 	/** Tijdstip backup database */
-	const TS_BACKUP_DB = '04:00';
+	private const TS_BACKUP_DB = '04:00';
 
 	/** Tijdstip backup bestanden */
-	const TS_BACKUP_FILES = '04:30';
+	private const TS_BACKUP_FILES = '04:30';
 
-	/** Init */
-	public static function init() {
+	#[Filter( 'updraftplus_blog_name' )]
+	/** Zet blognaam (voor in bestandsnaam backup) */
+	public function set_blog_name(): string {
+		return sanitize_title( SIW_SITE_NAME );
+	}
 
-		if ( ! is_plugin_active( 'updraftplus/updraftplus.php' ) ) {
-			return;
-		}
-		$self = new self();
-		add_filter( 'updraftplus_schedule_firsttime_db', [ $self, 'set_time_db_backup' ] );
-		add_filter( 'updraftplus_schedule_firsttime_files', [ $self, 'set_time_files_backup' ] );
-		add_filter( 'updraftplus_blog_name', fn() : string => sanitize_title( SIW_SITE_NAME ) );
-		add_action( 'admin_init', [ $self, 'hide_woocommerce_in_plugin_update_message' ], PHP_INT_MAX );
-
+	#[Action( 'init' )]
+	/** Definieer constantes */
+	public function define_constants() {
 		// Verbergt diverse UpdraftPlus notificaties
 		define( 'UPDRAFTPLUS_NOADS_B', true ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
 		define( 'UPDRAFTPLUS_NONEWSFEED', true ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
@@ -35,6 +36,7 @@ class UpdraftPlus {
 		define( 'UPDRAFTPLUS_DISABLE_WP_CRON_NOTICE', true ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
 	}
 
+	#[Filter( 'updraftplus_schedule_firsttime_db' )]
 	/** Zet de tijd voor de database backup */
 	public function set_time_db_backup( int $scheduled_time ): int {
 		$tomorrow = strtotime( 'tomorrow' );
@@ -42,6 +44,7 @@ class UpdraftPlus {
 		return strtotime( $backup_db_day . ' ' . self::TS_BACKUP_DB . wp_timezone_string() );
 	}
 
+	#[Filter( 'updraftplus_schedule_firsttime_files' )]
 	/** Zet de tijd voor de bestandsbackup */
 	public function set_time_files_backup( int $scheduled_time ): int {
 		$tomorrow = strtotime( 'tomorrow' );
@@ -49,6 +52,7 @@ class UpdraftPlus {
 		return strtotime( $backup_files_day . ' ' . self::TS_BACKUP_FILES . wp_timezone_string() );
 	}
 
+	#[Action( 'admin_init' )]
 	/** Verbergt melding bij WooCommerce plugin updates */
 	public function hide_woocommerce_in_plugin_update_message() {
 		global $updraftplus_admin;
