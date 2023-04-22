99ba90d0326520cd8d69972edc85ed362f8a02ef
Maarten <14947039+ictbeheer@users.noreply.github.com>
2023-04-22 21:50:50 +0200

    Bugfix social network cache

--- includes/functions/social-networks.php
+++ includes/functions/social-networks.php
@@ -16,7 +16,7 @@ use SIW\Data\Social_Network_Context;
  */
 function siw_get_social_networks( Social_Network_Context $context = Social_Network_Context::ALL ): array {
 
-	$social_networks = wp_cache_get( $context, __FUNCTION__ );
+	$social_networks = wp_cache_get( $context->value, __FUNCTION__ );
 	if ( false !== $social_networks ) {
 		return $social_networks;
 	}
@@ -39,7 +39,7 @@ function siw_get_social_networks( Social_Network_Context $context = Social_Netwo
 		$social_networks,
 		fn( Social_Network $social_network ): bool => $social_network->is_valid_for_context( $context )
 	);
-	wp_cache_set( $context, $social_networks, __FUNCTION__ );
+	wp_cache_set( $context->value, $social_networks, __FUNCTION__ );
 
 	return $social_networks;
 }
