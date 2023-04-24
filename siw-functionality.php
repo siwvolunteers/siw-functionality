d6239265b4343d432d2abeca257a51e269b9f877
Maarten <14947039+ictbeheer@users.noreply.github.com>
2023-04-24 22:06:03 +0200

    Minimum wp versie ophogen

--- siw-functionality.php
+++ siw-functionality.php
@@ -21,7 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Author URI:        https://www.siw.nl
  * Text Domain:       siw
  * License:           GPLv2 or later
- * Requires at least: 5.9
+ * Requires at least: 6.2
  * Requires PHP:      8.0
  */
 
