48905e1aead5b3255516275b7bd9f4a1c564a204
Maarten <14947039+ictbeheer@users.noreply.github.com>
2023-07-18 01:06:39 +0200

    Loader voor externe assets

---
+++ includes/external-assets/loader.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace SIW\External_Assets;
+
+use SIW\Abstracts\Base_Loader;
+
+/**
+ * Loader voor modules
+ *
+ * @copyright 2021 SIW Internationale Vrijwilligersprojecten
+ */
+class Loader extends Base_Loader {
+
+	/** {@inheritDoc} */
+	public function get_classes(): array {
+		return [];
+	}
+}
