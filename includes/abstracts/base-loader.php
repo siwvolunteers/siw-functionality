2e88be96e2f7b19858402cca1bea4b938c48c7d6
Maarten <14947039+ictbeheer@users.noreply.github.com>
2023-10-25 19:38:04 +0200

    Logging in Base loader

--- includes/abstracts/base-loader.php
+++ includes/abstracts/base-loader.php
@@ -3,6 +3,7 @@
 namespace SIW\Abstracts;
 
 use SIW\Base;
+use SIW\Util\Logger;
 
 /**
  * Roept load functie aan voor classes die SIW\Base extenden
@@ -13,14 +14,18 @@ abstract class Base_Loader extends Loader {
 
 	/** Laadt klasses */
 	protected function load_classes( array $classes ) {
-		array_walk( $classes, fn( string $class ) => $this->load( $class ) );
+		array_walk( $classes, fn( string $class_name ) => $this->load( $class_name ) );
 	}
 
 	/** Laadt 1 klasse */
-	final protected function load( string $class ) {
-		if ( ! is_a( $class, Base::class, true ) ) {
+	final protected function load( string $class_name ) {
+		if ( ! is_a( $class_name, Base::class, true ) ) {
+			Logger::warning(
+				sprintf( '%s extend niet %s', $class_name, Base::class ),
+				static::class
+			);
 			return;
 		}
-		$class::init();
+		$class_name::init();
 	}
 }
