5a330be3f3838fd29ac1754043b553cf5ece7bfa
Maarten <14947039+ictbeheer@users.noreply.github.com>
2024-01-13 00:22:58 +0100

    Safe redirect manager uitfaseren

--- includes/compatibility/loader.php
+++ includes/compatibility/loader.php
@@ -19,7 +19,6 @@ class Loader extends Class_Loader_Abstract {
 			GeneratePress::class,
 			Members::class,
 			Meta_Box::class,
-			Safe_Redirect_Manager::class,
 			SiteOrigin_Page_Builder::class,
 			SiteOrigin_Widgets_Bundle::class,
 			WooCommerce::class,
