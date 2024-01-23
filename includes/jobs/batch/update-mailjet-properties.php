877cd5ac21dc1fdecbf786b9788cf56a05957165
Maarten <14947039+ictbeheer@users.noreply.github.com>
2024-01-23 21:27:44 +0100

    Bugfix mailjet properties job

--- includes/jobs/batch/update-mailjet-properties.php
+++ includes/jobs/batch/update-mailjet-properties.php
@@ -4,6 +4,7 @@ namespace SIW\Jobs\Batch;
 
 use SIW\Attributes\Add_Action;
 use SIW\Integrations\Mailjet;
+use SIW\Integrations\Mailjet\Property;
 use SIW\Jobs\Update_Job;
 
 class Update_Mailjet_Properties extends Update_Job {
@@ -17,13 +18,14 @@ class Update_Mailjet_Properties extends Update_Job {
 
 	public function start(): void {
 		$existing_properties = Mailjet::create()->retrieve_properties( 'name' );
-		$missing_properties = array_diff_key( Mailjet::PROPERTIES, $existing_properties );
+		$properties = array_column( Property::cases(), 'value' );
+		$missing_properties = array_diff_key( $properties, $existing_properties );
 		$this->enqueue_items( array_keys( $missing_properties ), self::ACTION_HOOK );
 	}
 
 	#[Add_Action( self::ACTION_HOOK )]
 	public function create_property( string $property ) {
-		$datatype = Mailjet::PROPERTIES[ $property ];
+		$datatype = Property::from( $property )->get_data_type();
 		Mailjet::create()->create_property( $property, $datatype );
 	}
 }
