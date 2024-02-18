215d67455fa629412d06a9f82a487e780f73eb5f
Maarten <14947039+ictbeheer@users.noreply.github.com>
2024-02-18 09:34:28 +0100

    move address fields

--- includes/woocommerce/checkout/address-fields.php
+++ includes/woocommerce/checkout/address-fields.php
@@ -3,6 +3,8 @@ namespace SIW\WooCommerce\Checkout;
 
 use SIW\Attributes\Add_Filter;
 use SIW\Base;
+use SIW\Data\Gender;
+use SIW\Data\Nationality;
 
 class Address_Fields extends Base {
 
@@ -12,7 +14,6 @@ class Address_Fields extends Base {
 	#[Add_Filter( 'woocommerce_default_address_fields' )]
 	public function set_default_address_fields( array $default_address_fields ): array {
 
-		/* Verwijderen standaardvelden */
 		unset( $default_address_fields['address_1'] );
 		unset( $default_address_fields['address_2'] );
 		unset( $default_address_fields['postcode'] );
@@ -21,7 +22,6 @@ class Address_Fields extends Base {
 		unset( $default_address_fields['state'] );
 		unset( $default_address_fields['country'] );
 
-		/* Reset alle classes */
 		$default_address_fields = array_map(
 			function ( $field ) {
 				$field['class'] = array_diff( $field['class'], [ 'form-row-first', 'form-row-last', 'form-row-wide' ] );
@@ -30,10 +30,7 @@ class Address_Fields extends Base {
 			$default_address_fields
 		);
 
-		$address_fields = siw_get_data( 'workcamps/address-fields' );
-		$address_fields = wp_parse_args_recursive( $address_fields, $default_address_fields );
-
-		return $address_fields;
+		return wp_parse_args_recursive( $this->get_address_fields(), $default_address_fields );
 	}
 
 	#[Add_Filter( 'woocommerce_billing_fields' )]
@@ -41,5 +38,54 @@ class Address_Fields extends Base {
 		$billing_fields['billing_phone']['class'] = [ 'form-row-first' ];
 		$billing_fields['billing_email']['class'] = [ 'form-row-last' ];
 		return $billing_fields;
+	}
+
+	public function get_address_fields(): array {
+		return [
+			'first_name'  => [
+				'class'    => [ 'form-row-first' ],
+				'priority' => 10,
+			],
+			'last_name'   => [
+				'class'    => [ 'form-row-last' ],
+				'priority' => 20,
+			],
+			'dob'         => [
+				'label'    => __( 'Geboortedatum', 'siw' ),
+				'required' => true,
+				'type'     => 'date',
+				'class'    => [ 'form-row-first', 'update_totals_on_change' ],
+				'priority' => 30,
+			],
+			'nationality' => [
+				'label'    => __( 'Nationaliteit', 'siw' ),
+				'required' => true,
+				'type'     => 'select',
+				'options'  => Nationality::list(),
+				'default'  => Nationality::HOL->value,
+				'class'    => [ 'form-row-last' ],
+				'priority' => 40,
+			],
+			'gender'      => [
+				'label'    => __( 'Geslacht', 'siw' ),
+				'required' => true,
+				'type'     => 'radio',
+				'options'  => Gender::list(),
+				'class'    => [ 'form-row-first' ],
+				'priority' => 50,
+			],
+			'student'     => [
+				'label'    => __( 'Ben je student?', 'siw' ),
+				'type'     => 'radio',
+				'class'    => [ 'form-row-last', 'update_totals_on_change' ],
+				'options'  => [
+					'yes' => __( 'Ja', 'siw' ),
+					'no'  => __( 'Nee', 'siw' ),
+				],
+				'default'  => 'no',
+				'priority' => 60,
+			],
+
+		];
 	}
 }
