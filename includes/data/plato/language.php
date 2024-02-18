5cbe27228587ce573ffb8abe8f417bbb7edf09e6
Maarten <14947039+ictbeheer@users.noreply.github.com>
2024-02-18 15:30:48 +0100

    finish plato enums

--- includes/data/plato/language.php
+++ includes/data/plato/language.php
@@ -3,7 +3,6 @@
 namespace SIW\Data\Plato;
 
 use SIW\Data\Language as Language_Entity;
-use SIW\Interfaces\Enums\Labels;
 
 enum Language: string {
 
@@ -34,32 +33,32 @@ enum Language: string {
 	case TUR = 'TUR';
 	case SWE = 'SWE';
 
-	public function to_entity(): Labels {
+	public function to_entity(): Language_Entity {
 		return match ( $this ) {
-			self::ARA     => Language_Entity::ARABIC,
-			self::CAT    => Language_Entity::CATALAN,
-			self::CHN    => Language_Entity::CHINESE,
-			self::DNK     => Language_Entity::DANISH,
-			self::GER     => Language_Entity::GERMAN,
-			self::ENG    => Language_Entity::ENGLISH,
-			self::EST   => Language_Entity::ESTONIAN,
-			self::FIN    => Language_Entity::FINNISH,
-			self::FRA     => Language_Entity::FRENCH,
-			self::GRE      => Language_Entity::GREEK,
-			self::HEB     => Language_Entity::HEBREW,
-			self::ITA    => Language_Entity::ITALIAN,
-			self::JAP   => Language_Entity::JAPANESE,
-			self::KOR     => Language_Entity::KOREAN,
-			self::HOL      => Language_Entity::DUTCH,
-			self::UKR  => Language_Entity::UKRAINIAN,
-			self::POL     => Language_Entity::POLISH,
+			self::ARA => Language_Entity::ARABIC,
+			self::CAT => Language_Entity::CATALAN,
+			self::CHN => Language_Entity::CHINESE,
+			self::DNK => Language_Entity::DANISH,
+			self::GER => Language_Entity::GERMAN,
+			self::ENG => Language_Entity::ENGLISH,
+			self::EST => Language_Entity::ESTONIAN,
+			self::FIN => Language_Entity::FINNISH,
+			self::FRA => Language_Entity::FRENCH,
+			self::GRE => Language_Entity::GREEK,
+			self::HEB => Language_Entity::HEBREW,
+			self::ITA => Language_Entity::ITALIAN,
+			self::JAP => Language_Entity::JAPANESE,
+			self::KOR => Language_Entity::KOREAN,
+			self::HOL => Language_Entity::DUTCH,
+			self::UKR => Language_Entity::UKRAINIAN,
+			self::POL => Language_Entity::POLISH,
 			self::POR => Language_Entity::PORTUGUESE,
-			self::RUS    => Language_Entity::RUSSIAN,
-			self::SLK     => Language_Entity::SLOWAK,
-			self::ESP    => Language_Entity::SPANISH,
-			self::CZE      => Language_Entity::CZECH,
-			self::TUR    => Language_Entity::TURKISH,
-			self::SWE    => Language_Entity::SWEDISH,
+			self::RUS => Language_Entity::RUSSIAN,
+			self::SLK => Language_Entity::SLOWAK,
+			self::ESP => Language_Entity::SPANISH,
+			self::CZE => Language_Entity::CZECH,
+			self::TUR => Language_Entity::TURKISH,
+			self::SWE => Language_Entity::SWEDISH,
 		};
 	}
 }
