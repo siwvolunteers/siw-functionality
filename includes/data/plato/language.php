52d1e7bcc6d15f583efa07d830c2f20131b7e131
Maarten <14947039+ictbeheer@users.noreply.github.com>
2024-02-18 15:10:16 +0100

    Update language

--- includes/data/plato/language.php
+++ includes/data/plato/language.php
@@ -9,57 +9,57 @@ enum Language: string {
 
 	use Entity;
 
-	case ARABIC     = 'ARA';
-	case CATALAN    = 'CAT';
-	case CHINESE    = 'CHN';
-	case DANISH     = 'DNK';
-	case GERMAN     = 'GER';
-	case ENGLISH    = 'ENG';
-	case ESTONIAN   = 'EST';
-	case FINNISH    = 'FIN';
-	case FRENCH     = 'FRA';
-	case GREEK      = 'GRE';
-	case HEBREW     = 'HEB';
-	case ITALIAN    = 'ITA';
-	case JAPANESE   = 'JAP';
-	case KOREAN     = 'KOR';
-	case DUTCH      = 'HOL';
-	case UKRAINIAN  = 'UKR';
-	case POLISH     = 'POL';
-	case PORTUGUESE = 'POR';
-	case RUSSIAN    = 'RUS';
-	case SLOWAK     = 'SLK';
-	case SPANISH    = 'ESP';
-	case CZECH      = 'CZE';
-	case TURKISH    = 'TUR';
-	case SWEDISH    = 'SWE';
+	case ARA = 'ARA';
+	case CAT = 'CAT';
+	case CHN = 'CHN';
+	case DNK = 'DNK';
+	case GER = 'GER';
+	case ENG = 'ENG';
+	case EST = 'EST';
+	case FIN = 'FIN';
+	case FRA = 'FRA';
+	case GRE = 'GRE';
+	case HEB = 'HEB';
+	case ITA = 'ITA';
+	case JAP = 'JAP';
+	case KOR = 'KOR';
+	case HOL = 'HOL';
+	case UKR = 'UKR';
+	case POL = 'POL';
+	case POR = 'POR';
+	case RUS = 'RUS';
+	case SLK = 'SLK';
+	case ESP = 'ESP';
+	case CZE = 'CZE';
+	case TUR = 'TUR';
+	case SWE = 'SWE';
 
 	public function to_entity(): Labels {
 		return match ( $this ) {
-			self::ARABIC     => Language_Entity::ARABIC,
-			self::CATALAN    => Language_Entity::CATALAN,
-			self::CHINESE    => Language_Entity::CHINESE,
-			self::DANISH     => Language_Entity::DANISH,
-			self::GERMAN     => Language_Entity::GERMAN,
-			self::ENGLISH    => Language_Entity::ENGLISH,
-			self::ESTONIAN   => Language_Entity::ESTONIAN,
-			self::FINNISH    => Language_Entity::FINNISH,
-			self::FRENCH     => Language_Entity::FRENCH,
-			self::GREEK      => Language_Entity::GREEK,
-			self::HEBREW     => Language_Entity::HEBREW,
-			self::ITALIAN    => Language_Entity::ITALIAN,
-			self::JAPANESE   => Language_Entity::JAPANESE,
-			self::KOREAN     => Language_Entity::KOREAN,
-			self::DUTCH      => Language_Entity::DUTCH,
-			self::UKRAINIAN  => Language_Entity::UKRAINIAN,
-			self::POLISH     => Language_Entity::POLISH,
-			self::PORTUGUESE => Language_Entity::PORTUGUESE,
-			self::RUSSIAN    => Language_Entity::RUSSIAN,
-			self::SLOWAK     => Language_Entity::SLOWAK,
-			self::SPANISH    => Language_Entity::SPANISH,
-			self::CZECH      => Language_Entity::CZECH,
-			self::TURKISH    => Language_Entity::TURKISH,
-			self::SWEDISH    => Language_Entity::SWEDISH,
+			self::ARA     => Language_Entity::ARABIC,
+			self::CAT    => Language_Entity::CATALAN,
+			self::CHN    => Language_Entity::CHINESE,
+			self::DNK     => Language_Entity::DANISH,
+			self::GER     => Language_Entity::GERMAN,
+			self::ENG    => Language_Entity::ENGLISH,
+			self::EST   => Language_Entity::ESTONIAN,
+			self::FIN    => Language_Entity::FINNISH,
+			self::FRA     => Language_Entity::FRENCH,
+			self::GRE      => Language_Entity::GREEK,
+			self::HEB     => Language_Entity::HEBREW,
+			self::ITA    => Language_Entity::ITALIAN,
+			self::JAP   => Language_Entity::JAPANESE,
+			self::KOR     => Language_Entity::KOREAN,
+			self::HOL      => Language_Entity::DUTCH,
+			self::UKR  => Language_Entity::UKRAINIAN,
+			self::POL     => Language_Entity::POLISH,
+			self::POR => Language_Entity::PORTUGUESE,
+			self::RUS    => Language_Entity::RUSSIAN,
+			self::SLK     => Language_Entity::SLOWAK,
+			self::ESP    => Language_Entity::SPANISH,
+			self::CZE      => Language_Entity::CZECH,
+			self::TUR    => Language_Entity::TURKISH,
+			self::SWE    => Language_Entity::SWEDISH,
 		};
 	}
 }
