3169e187a0d3b79b60da3c1845faf34724c31762
Maarten <14947039+ictbeheer@users.noreply.github.com>
2024-03-16 13:26:30 +0100

    (fix) update logger

--- includes/util/logger.php
+++ includes/util/logger.php
@@ -6,43 +6,44 @@ use SIW\Facades\WooCommerce;
 
 class Logger {
 
-	protected static function log( string $message, string $level, string $source = 'siw' ) {
+	protected static function log( string $message, string $level, string $source, array $context = [] ) {
 		if ( ! \WC_Log_Levels::is_valid_level( $level ) ) {
 			$level = \WC_Log_Levels::INFO;
 		}
 
-		WooCommerce::get_logger()?->log( $level, $message, [ 'source' => $source ] );
+		$context['source'] = str_replace( '\\', '-', $source );
+		WooCommerce::get_logger()?->log( $level, $message, $context );
 	}
 
-	public static function debug( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::DEBUG, $source );
+	public static function debug( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::DEBUG, $source, $context );
 	}
 
-	public static function info( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::INFO, $source );
+	public static function info( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::INFO, $source, $context );
 	}
 
-	public static function notice( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::NOTICE, $source );
+	public static function notice( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::NOTICE, $source, $context );
 	}
 
-	public static function warning( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::WARNING, $source );
+	public static function warning( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::WARNING, $source, $context );
 	}
 
-	public static function error( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::ERROR, $source );
+	public static function error( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::ERROR, $source, $context );
 	}
 
-	public static function critical( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::CRITICAL, $source );
+	public static function critical( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::CRITICAL, $source, $context );
 	}
 
-	public static function alert( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::ALERT, $source );
+	public static function alert( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::ALERT, $source, $context );
 	}
 
-	public static function emergency( string $message, string $source ) {
-		self::log( $message, \WC_Log_Levels::EMERGENCY, $source );
+	public static function emergency( string $message, string $source, array $context = [] ) {
+		self::log( $message, \WC_Log_Levels::EMERGENCY, $source, $context );
 	}
 }
